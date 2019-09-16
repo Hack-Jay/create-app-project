d9836cbc8bbf7edded0f5b62f0c7c7d1f409e4ec
Hack-Jay <735059024@qq.com>
2019-09-16 23:35:12 +0800

    login page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ReactDom from "react-dom";
+import 'normalize.css'
 import axios from "axios";
 import "./index.less";
+import Login from './views/Login'
 
 function App() {
   const [list, setList] = useState([]);
@@ -16,4 +18,4 @@ function App() {
   return <h3>Hello React</h3>;
 }
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<Login />, document.getElementById("root"));
---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { BrowserRouter, Switch, Router, Route } from 'react-router-dom'
+
+
+function Layout() {
+
+}
+
+export default Layout
